7e2bacbbbe30cfa521d16e08c85371480eed575e
surajattar <surajattar17@gmail.com>
2023-08-24 16:57:41 +0530

    initial version

---
+++ precache-manifest.38d5dccc9c4302a0a74324dfaf20cc2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc8d93efd09c6ed638f",
+    "url": "/static/css/main.a35dd536.chunk.css"
+  },
+  {
+    "revision": "0dc8d93efd09c6ed638f",
+    "url": "/static/js/main.b30222f2.chunk.js"
+  },
+  {
+    "revision": "60e406d5dbb3cad0901c",
+    "url": "/static/js/runtime~main.48b17165.js"
+  },
+  {
+    "revision": "8c45e320f724366a6ada",
+    "url": "/static/js/2.da81e6b2.chunk.js"
+  },
+  {
+    "revision": "5854eb9be6dd082bd048",
+    "url": "/static/js/3.524ce66f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c7f14a5113dde6572196a23d6d936976",
+    "url": "/index.html"
+  }
+];
